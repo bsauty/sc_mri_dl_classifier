9bd6fc7cc022493b7749b6b44fc4f1b9a44a826c
bsauty <42009161+bsauty@users.noreply.github.com>
2019-06-13 11:29:48 -0400

    change train_classifier script

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'classify=classifier.main:run_main',
+            'train_classifier=classifier.main:run_main',
             'classify_acquisition=classifier.classify:run_main',
         ],
     },
